b0300bc475598a444eb3b9e81f0c4cd80a1e742e
James Williams <james.williams@liquid-armour.com>
2025-08-30 17:44:02 +0100

    Initial Commit

---
+++ infinordle/infinordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  infinordleApp.swift
+//  infinordle
+//
+//  Created by James Williams on 30/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct infinordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ infinordleTests/infinordleTests.swift
@@ -0,0 +1,16 @@
+//
+//  infinordleTests.swift
+//  infinordleTests
+//
+//  Created by James Williams on 30/08/2025.
+//
+
+import Testing
+
+struct infinordleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
